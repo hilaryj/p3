2fc74b55e93d28ac57275b98815569e6e5375d40
hilaryj <hilaryj@users.noreply.github.com>
2014-10-17 13:13:38 -0400

    working on user generator; logic still broken

--- app/routes.php
+++ app/routes.php
@@ -21,7 +21,31 @@ Route::post('/loremipsum', function(){
 
 # Random User Generator get & post
 Route::get('/usergenerator', function(){
-	return View::make('usergenerator');
+
+	# Get data from form
+	$usernumber = Input::Get('users');
+	$bday = Input::Get('birthday');
+	$tag = Input::Get('tagline');
+	$profileinfo = ''; #empty for now
+
+	# Set up Faker generator
+	$faker = new Faker\Generator();
+	#$faker = Faker::create();
+
+	# Now what?
+	# Given the num of requested users do a for loop echoing the relevant information...
+	for($i=0; $i<$usernumber; $i++) {
+		$profileinfo .= '<p>' . $faker->name . '</p>';
+		$profileinfo .= '<p>' . $faker->dateTimeThisCentury->format(mm-dd-y) . '</p>';
+		$profileinfo .= '<p>' . $faker->sentence($nbWords = 12) . '</p>' ; 
+	}
+	#using p to do block element
+
+	#Return generated users
+	return View::make('usergenerator')
+		->with('users',$usernumber)
+		->with('birthday',$bday)
+		->with('tagline' $tag)
 });
 Route::post('/usergenerator', function(){
 
--- app/views/usergenerator.blade.php
+++ app/views/usergenerator.blade.php
@@ -6,11 +6,27 @@
 
 @section('content')
 	<h1>User Generator</h1>
-	<?php
-	# Instantiate a new RandomStringGenerator Object
-	$string_generator = new RndStr();
 
-	# Use the generate() method on this object
-	echo $string_generator->generate(10);
-	?>
+	{{ Form::open(array('url' => '/loremipsum', 'method' => 'GET')) }}
+	{{ Form::label('users','How many users?') }}
+	{{ Form::text('users'); }}
+	<br>
+	{{ Form::label('birthday', 'Include birthday?'); }}
+	{{ Form::checkbox('birthday', 'value'); }}
+	<br>
+	{{ Form::label('tagline', 'Include tagline?'); }}
+	{{ Form::checkbox('tagline', 'value'); }}
+	<br>
+	{{ Form::submit('Generate users!') }}
+	{{ Form::close() }}
+
+	@foreach($users as $user => $result)
+		<p>{{ $result }}</p>
+	@endforeach
+	@foreach
+	@endforeach
+	@foreach
+	@endforeach
+
+
 @stop <!-- this is the content section -->
